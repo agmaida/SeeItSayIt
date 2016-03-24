185bcbd81f3e25a371d3d21bafeb06b614784e95
andydng <andydng@hotmail.com>
2016-03-24 19:16:00 -0400

    Added User and UserLocalStore

---
+++ app/src/main/java/com/example/andrew/seeitsayit/User.java
@@ -0,0 +1,16 @@
+package com.example.andrew.seeitsayit;
+
+/**
+ * Created by andyd on 3/24/2016.
+ */
+public class User {
+
+    String email, password;
+
+    public User(String email, String password)
+    {
+        this.email = email;
+        this.password = password;
+    }
+
+}
