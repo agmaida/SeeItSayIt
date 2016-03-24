47e93ebb9978f09673e1d5951c66219042780f1a
agmaida <agmaida@knights.ucf.edu>
2016-03-24 18:02:01 -0400

    first upload of all files

---
+++ app/src/main/java/com/example/andrew/seeitsayit/ResetPassword.java
@@ -0,0 +1,13 @@
+package com.example.andrew.seeitsayit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ResetPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+    }
+}
