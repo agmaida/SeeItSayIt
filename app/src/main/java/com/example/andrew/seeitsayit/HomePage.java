d5e249ed72495edc62a27c640e353cf9e5315569
mgarro385 <mgarro385@knights.ucf.edu>
2016-03-29 19:46:34 -0400

    Added navigation contorl for going to the activity_create_ticket

--- app/src/main/java/com/example/andrew/seeitsayit/HomePage.java
+++ app/src/main/java/com/example/andrew/seeitsayit/HomePage.java
@@ -1,7 +1,10 @@
 package com.example.andrew.seeitsayit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomePage extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        //This code with the lambda statements is the code for navigation from the Home Page to the Create Ticket page via the Create Ticket button
+        Button createTicket = (Button)findViewById(R.id.homeCreateTicket);
+        createTicket.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomePage.this, CreateTicket.class));
+            }
+        });
     }
 }
