5e0803b333fd4d9d0a9fb753b47d60b9264869ac
andydng <andydng@hotmail.com>
2016-04-11 19:54:45 -0400

    Editing ServerRequests

--- app/src/main/java/com/example/andrew/seeitsayit/ServerRequests.java
+++ app/src/main/java/com/example/andrew/seeitsayit/ServerRequests.java
@@ -7,15 +7,10 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.net.Uri;
 import android.os.AsyncTask;
-import android.util.Log;
 
-import org.apache.http.params.HttpConnectionParams;
-import org.apache.http.params.HttpParams;
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -23,15 +18,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLEncoder;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.net.ssl.HttpsURLConnection;
 
 public class ServerRequests {
     ProgressDialog progressDialog;
