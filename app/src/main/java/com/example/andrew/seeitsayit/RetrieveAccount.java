5fc3dafac3acc881274e50833112886a499d6011
agmaida <agmaida@knights.ucf.edu>
2016-04-12 20:18:07 -0400

    -fixed Retrieve Account errors

--- app/src/main/java/com/example/andrew/seeitsayit/RetrieveAccount.java
+++ app/src/main/java/com/example/andrew/seeitsayit/RetrieveAccount.java
@@ -1,8 +1,8 @@
 package com.example.andrew.seeitsayit;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+//import android.support.design.widget.FloatingActionButton;
+//import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
