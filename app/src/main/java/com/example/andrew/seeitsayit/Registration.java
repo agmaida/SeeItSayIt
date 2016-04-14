afe45ddfa6b2168dd22da10f100d515f85bd5f5d
Jessie_Maraman <jessiemaraman@gmail.com>
2016-04-14 17:03:04 -0400

    Started the registration page

--- app/src/main/java/com/example/andrew/seeitsayit/Registration.java
+++ app/src/main/java/com/example/andrew/seeitsayit/Registration.java
@@ -2,13 +2,33 @@ package com.example.andrew.seeitsayit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Registration extends AppCompatActivity {
+
+    Button registerSubmit;
+    EditText regUsername, regEmail, regPassword, regPasswordConfirm;
+
+    UserLocalStore userLocalStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        //registerSubmit = (Button) findViewById(R.id.registerSubmit);
+
+        //registerSubmit.setOnClickListener(this);
+
+        //regUsername = (EditText) findViewById(R.id.registerUsername);
+        //regEmail = (EditText) findViewById(R.id.registerEmail);
+        //regPassword = (EditText) findViewById(R.id.registerPassword);
+        //regPasswordConfirm = (EditText) findViewById(R.id.registerPasswordConfirm);
+
+
+        //userLocalStore = new UserLocalStore(this);
     }
 
 
