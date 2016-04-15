e1118ff8579a37659cf605c8577b7e7865d95840
Michael Garro <mgarro385@knights.ucf.edu>
2016-04-15 18:48:43 -0400

    Finished Create Ticket Still need to solve NullPointerException with photo

--- app/src/main/java/com/example/andrew/seeitsayit/CreateTicket.java
+++ app/src/main/java/com/example/andrew/seeitsayit/CreateTicket.java
@@ -1,13 +1,155 @@
 package com.example.andrew.seeitsayit;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class CreateTicket extends AppCompatActivity {
+
+    private static final String TAG = "CallCamera";
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQ = 0;
+
+    Uri fileUri = null;
+    ImageView photoImage = null;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_ticket);
+        photoImage = (ImageView) findViewById(R.id.photoImage);
+        photoImage.setImageDrawable(null);
+
+        Button callCameraButton = (Button) findViewById(R.id.btnAddPhoto);
+        callCameraButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                File file = getOutputPhotoFile();
+                fileUri = Uri.fromFile(getOutputPhotoFile());
+                i.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+                startActivityForResult(i, CAPTURE_IMAGE_ACTIVITY_REQ);
+            }
+        });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    private File getOutputPhotoFile() {
+        File directory = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), getPackageName());
+        if (!directory.exists()) {
+            if (!directory.mkdirs()) {
+                Log.e(TAG, "Failed to create storage directory.");
+                return null;
+            }
+        }
+        String timeStamp = new SimpleDateFormat("yyyMMdd_HHmmss").format(new Date());
+        return new File(directory.getPath() + File.separator + "IMG_"
+                + timeStamp + ".jpg");
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQ) {
+            if (resultCode == RESULT_OK) {
+                Uri photoUri = null;
+                if (data == null) {
+                    // A known bug here! The image should have saved in fileUri
+                    Toast.makeText(this, "Image saved successfully",
+                            Toast.LENGTH_LONG).show();
+                    photoUri = fileUri;
+                } else {
+                    photoUri = data.getData();
+                    Toast.makeText(this, "Image saved successfully in: " + data.getData(),
+                            Toast.LENGTH_LONG).show();
+                }
+                showPhoto(photoUri);
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Callout for image capture failed!",
+                        Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private void showPhoto(Uri photoUri) {
+        File imageFile = new File(photoUri.getPath());
+        if (imageFile.exists()) {
+            ((BitmapDrawable) photoImage.getDrawable()).getBitmap().recycle();
+            Drawable oldDrawable = photoImage.getDrawable();
+            if (oldDrawable != null)
+            {
+                ((BitmapDrawable)oldDrawable).getBitmap().recycle();
+            }
+            Bitmap bitmap = BitmapFactory.decodeFile(imageFile.getAbsolutePath());
+            BitmapDrawable drawable = new BitmapDrawable(this.getResources(), bitmap);
+            photoImage.setScaleType(ImageView.ScaleType.FIT_CENTER);
+            photoImage.setImageDrawable(drawable);
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "CreateTicket Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.andrew.seeitsayit/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "CreateTicket Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.andrew.seeitsayit/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
