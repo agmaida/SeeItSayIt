3308457d051b87f176ba8d5da488d81cd0cfa104
Jessie_Maraman <jessiemaraman@gmail.com>
2016-04-14 20:23:33 -0400

    Disclaimer: This branch is broken! Do Not Pull!
    Started the registering a new user code. Currently not working!

--- app/src/main/java/com/example/andrew/seeitsayit/Registration.java
+++ app/src/main/java/com/example/andrew/seeitsayit/Registration.java
@@ -1,15 +1,17 @@
 package com.example.andrew.seeitsayit;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class Registration extends AppCompatActivity {
+public class Registration extends AppCompatActivity implements View.OnClickListener{
 
     Button registerSubmit;
-    EditText regUsername, regEmail, regPassword, regPasswordConfirm;
+    EditText registerEmail, registerPassword, registerPasswordConfirm;
 
     UserLocalStore userLocalStore;
 
@@ -18,18 +20,60 @@ public class Registration extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
-        //registerSubmit = (Button) findViewById(R.id.registerSubmit);
+        registerSubmit = (Button) findViewById(R.id.registerSubmit);
+        registerSubmit.setOnClickListener(this);
 
-        //registerSubmit.setOnClickListener(this);
-
-        //regUsername = (EditText) findViewById(R.id.registerUsername);
-        //regEmail = (EditText) findViewById(R.id.registerEmail);
-        //regPassword = (EditText) findViewById(R.id.registerPassword);
-        //regPasswordConfirm = (EditText) findViewById(R.id.registerPasswordConfirm);
+        registerEmail = (EditText) findViewById(R.id.registerEmail);
+        registerPassword = (EditText) findViewById(R.id.registerPassword);
+        registerPasswordConfirm = (EditText) findViewById(R.id.registerPasswordConfirm);
 
 
-        //userLocalStore = new UserLocalStore(this);
+        userLocalStore = new UserLocalStore(this);
     }
 
+    public void onClick(View view) {
+        switch (view.getId()) {
 
+            case R.id.registerPasswordConfirm:
+                String email = registerEmail.getText().toString();
+                String password1 = registerPassword.getText().toString();
+                String password2 = registerPasswordConfirm.getText().toString();
+
+                if (password1 == password2) {
+                    User user = new User(email, password1);
+                    createUser(user);
+                }
+                else{
+                    showErrorMessage();
+                }
+
+
+                break;
+
+        }
+    }
+
+    private void createUser(User user) {
+        ServerRequests serverRequest = new ServerRequests(this);
+        serverRequest.storeUserDataInBackground(user, new GetUserCallback() {
+            @Override
+            public void done(User returnedUser) {
+
+                saveUser(returnedUser);
+            }
+        });
+    }
+
+    private void saveUser(User returnedUser) {
+        userLocalStore.storeUserData(returnedUser);
+        userLocalStore.setUserLoggedIn(true);
+        startActivity(new Intent(this, HomePage.class)); //User goes to this when they log in
+    }
+
+    private void showErrorMessage() {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(Registration.this);
+        dialogBuilder.setMessage("Passwords do not match");
+        dialogBuilder.setPositiveButton("Ok", null);
+        dialogBuilder.show();
+    }
 }
--- app/src/main/java/com/example/andrew/seeitsayit/RetrieveAccount.java
+++ app/src/main/java/com/example/andrew/seeitsayit/RetrieveAccount.java
@@ -13,8 +13,8 @@ public class RetrieveAccount extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_retrieve_account);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
 
 
