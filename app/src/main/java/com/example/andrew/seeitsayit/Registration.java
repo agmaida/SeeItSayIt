42ced3ce6477ae6e93ae270cbea103fa6cae90b7
Jessie_Maraman <jessiemaraman@gmail.com>
2016-04-19 20:17:18 -0400

    Registration no longer allows empty fields.

--- app/src/main/java/com/example/andrew/seeitsayit/Registration.java
+++ app/src/main/java/com/example/andrew/seeitsayit/Registration.java
@@ -7,11 +7,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.util.Log;
 
 public class Registration extends AppCompatActivity  implements View.OnClickListener{
 
     EditText registerEmail, registerPassword, registerPasswordConfirm;
     Button registerSubmit;
+    private static final String Tag = "MyActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,17 +38,23 @@ public class Registration extends AppCompatActivity  implements View.OnClickList
 
                 User user = new User(email, password1);
 
-                if(!password1.equals(password2))
-                {
-                    showErrorMessage();
+                if(email.isEmpty() || password1.isEmpty() || password2.isEmpty()){
+                    emptyFieldErrorMessage();
                 }
+
+                else if(!password1.equals(password2) && password1.isEmpty() || password2.isEmpty())
+                {
+                    passwordErrorMessage();
+                }
+
                 else
                 {
-                    if (email.contains("@knights.ucf.edu") && email.contains("@ucf.edu")){
+
+                    if (email.contains("@knights.ucf.edu") || email.contains("@ucf.edu")){
                         registerUser(user);
                     }
                     else{
-                        showErrorMessage2();
+                        emailErrorMessage();
                     }
                 }
 
@@ -67,16 +75,23 @@ public class Registration extends AppCompatActivity  implements View.OnClickList
         });
     }
 
-    private void showErrorMessage() {
+    private void passwordErrorMessage() {
         AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(Registration.this);
         dialogBuilder.setMessage("Password do not match.");
         dialogBuilder.setPositiveButton("Ok", null);
         dialogBuilder.show();
     }
 
-    private void showErrorMessage2() {
+    private void emailErrorMessage() {
         AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(Registration.this);
-        dialogBuilder.setMessage("Email must a UCF email.");
+        dialogBuilder.setMessage("Email must be a UCF email.");
+        dialogBuilder.setPositiveButton("Ok", null);
+        dialogBuilder.show();
+    }
+
+    private void emptyFieldErrorMessage() {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(Registration.this);
+        dialogBuilder.setMessage("All fields must be filled.");
         dialogBuilder.setPositiveButton("Ok", null);
         dialogBuilder.show();
     }
